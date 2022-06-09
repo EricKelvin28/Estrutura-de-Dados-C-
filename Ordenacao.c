ff9f70b15483b7425e92691ef5d0c0820dc78e79
Eric <ekbs@icomp.ufam.edu.br>
2022-06-09 14:44:14 -0400

    corrigido

--- Ordenacao.c
+++ Ordenacao.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insert_short(int v[],int tam){
+void insertion_short(int v[],int tam){
     for (int j = 2; j < tam; j++)
     {
         int chave = v[j];
