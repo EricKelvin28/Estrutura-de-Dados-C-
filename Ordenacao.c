27fb8ef5e13d834822d6be141f2169bd1fde72e7
Eric <ekbs@icomp.ufam.edu.br>
2022-06-09 14:41:00 -0400

    insertion short criado

--- Ordenacao.c
+++ Ordenacao.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insert_short(void *v){
-    
+void insert_short(int v[],int tam){
+    for (int j = 2; j < tam; j++)
+    {
+        int chave = v[j];
+        int i = j-1;
+        while (i>0 && v[i]>chave)
+        {
+            v[i+1] = v[i];
+            i = i-1;
+        }
+        v[i+1] = chave;
+    }
 }
